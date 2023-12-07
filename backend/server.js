57397495d7a34c01b133fbb594838351053291d2
grima <gikandimaria178@gmail.com>
2023-12-07 19:53:54 +0300

    delete vercel

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
-mongoose.connect('mongodb+srv://maria:12345@cluster0.t1gpwj2.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
